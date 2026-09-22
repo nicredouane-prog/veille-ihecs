e68f152b86bbb2b897f5d1b7cbf27f444ba2e8fe
nicredouane-prog <nicredouane@gmail.com>
2026-09-22 22:12:07 +0200

    Delete veille-journalistique-ihecs directory

--- veille-journalistique-ihecs/sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE='veille-ihecs-v2.1';
-const CORE=['/','/manifest.webmanifest','/icon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))))});
-self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.matchAll({type:'window',includeUncontrolled:true}).then(list=>list[0]?list[0].focus():clients.openWindow('/')))});
